24ae6aee2641a72a578bf849798291677417e4a9
Chun (Cyrus) <cyrus1127@gmail.com>
2023-06-20 16:26:45 +0100

    Update button
    - Update : add all filtering button
    - Update : equipment wear and remove
    - Update : item filtering

--- scenes/GUI/HUD_characterInfo.gd
+++ scenes/GUI/HUD_characterInfo.gd
@@ -6,12 +6,15 @@ signal closed
 # var a = 2
 # var b = "text"
 enum CateType{
+	All,
 	Recover,
 	Equipment,
-	All
 }
-var itemIndex = 0
+
+var curSelectingType = CateType.All
+var itemIndex = -1
 var slots = []
+var onlistingItem = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,7 +29,7 @@ func _ready():
 #func _process(delta):
 #	pass
 func update():
-	changeListedItem(CateType.Recover)
+	changeListedItem(curSelectingType)
 	reviewEquipments()
 	# load player backpack records
 	print("itme count : " + str(GLOBAL.playerData.items.size()))
@@ -74,20 +77,32 @@ func changeListedItem(n_type : int):
 	#do clear all existing development items from list before add new
 	$item_info_rect/ScrollContainer/VScrollBar/ItemList.clear()
 	
+
 	#load item list
 	var allItems = getItems()
+	onlistingItem.clear()
 	if allItems:
+				
 		for nItem in allItems:
 			#{  "name": "Wine 2", "detail":"this is food. Recover SP 20", "price": 17,  "type": "Recover", "recoverType":"sp", "value":20 , "amt":0}
 			var texture
-			if nItem.type == "Equipment":
-				texture = GLOBAL.getEquipmentTexture(nItem.name)
-			elif nItem.type == "Weapon":
-				texture = GLOBAL.getEquipmentTexture(nItem.name, true)
-			else : 
-				texture = GLOBAL.getCommonItemTexture(nItem.name)
+			match (n_type):
+				CateType.All :
+					if (nItem.type == "Equipment" || nItem.type == "Weapon" ) :
+						if !isWearingEquipment(nItem):
+							texture = GLOBAL.getEquipmentTexture(nItem.name, nItem.type == "Weapon")
+					else :
+						texture = GLOBAL.getCommonItemTexture(nItem.name)
+				CateType.Recover:
+					if nItem.type == "Recover": 
+						texture = GLOBAL.getCommonItemTexture(nItem.name)
+				CateType.Equipment:
+					if (nItem.type == "Equipment" || nItem.type == "Weapon" ) && !isWearingEquipment(nItem) :
+						texture = GLOBAL.getEquipmentTexture(nItem.name, nItem.type == "Weapon")
+				
 				
 			if texture:
+				onlistingItem.push_back(nItem)
 				var amt = 1
 				if (nItem as Dictionary).keys().find('amt') != -1 :
 					amt = nItem.amt
@@ -95,6 +110,13 @@ func changeListedItem(n_type : int):
 		
 	pass
 
+func isWearingEquipment(item):
+	if (item.type == "Equipment" || item.type == "Weapon" ) :
+		if (item as Dictionary).keys().find('equiped') != -1 :
+			if item.equiped == true :
+				return true
+				
+	return false
 
 func _on_TextureButton_pressed():
 	emit_signal("closed")
@@ -105,36 +127,52 @@ func _on_TextureButton_pressed():
 func _on_ItemList_item_selected(index):
 	itemIndex = index
 
-	var allItems = getItems()
-	if allItems && itemIndex < allItems.size():
-		$Control/lbl_name.text = allItems[itemIndex].name
-		$Control/lbl_name2.text = allItems[itemIndex].detail
+	if onlistingItem.size() > 0 && itemIndex < onlistingItem.size():
+		$Control/lbl_name.text = onlistingItem[itemIndex].name
+		$Control/lbl_name2.text = onlistingItem[itemIndex].detail
 	
 	pass # Replace with function body.
 
 func _on_btn_use_button_down():
+	
 	var allItems = getItems()
-	if allItems:
+	
+	if  allItems && onlistingItem.size() > 0 && itemIndex > -1:
+		var curType = onlistingItem[itemIndex].type
+		var itemName = onlistingItem[itemIndex].name
+		var itemIndexInFullList = -1
 		
-		var curType = allItems[itemIndex].type
+		#search the item index in full list
+		for index in allItems.size():
+			var item = allItems[index]
+			if item.name.match(itemName) :
+				if curType.match("Recover") :
+					itemIndexInFullList = index
+					break
+				else :
+					if !isWearingEquipment(item) :
+						itemIndexInFullList = index
+						break
 		
-		if curType.match("Recover"):
-			var curAmt = allItems[itemIndex].amt
-			if curAmt - 1 == 0:
-				allItems.remove(itemIndex)
-				itemIndex = 0 #reset the selected index
-			else:
-				allItems[itemIndex].amt -= 1
-			GLOBAL.change_sfx("fooditemUse")
-		elif curType.match("Weapon") || curType.match("Equipment"):
-			print('should equip item')
-			if equipItem(allItems[itemIndex]) :
-				print('item equiped')	
-				(allItems[itemIndex] as Dictionary)["equiped"] = true
+		# handle found item
+		if itemIndexInFullList >= 0:
+			if curType.match("Recover"):
+				var curAmt = onlistingItem[itemIndex].amt
+				if curAmt - 1 == 0:
+					allItems.remove(itemIndexInFullList)
+					itemIndex = 0 #reset the selected index
+				else:
+					allItems[itemIndexInFullList].amt -= 1
+				GLOBAL.change_sfx("fooditemUse")
+			elif curType.match("Weapon") || curType.match("Equipment"):
+				print('should equip item')
+				if equipItem(allItems[itemIndexInFullList]) :
+					print('item equiped')	
+					(allItems[itemIndexInFullList] as Dictionary)["equiped"] = true
+				else :
+					print('item cant equip with some issues')	
 			else :
-				print('item cant equip with some issues')	
-		else :
-			print('other types')
+				print('other types')
 			
 		update()
 	pass # Replace with function body.
@@ -142,31 +180,58 @@ func _on_btn_use_button_down():
 func equipItem(data):
 	var id = data.id
 	if data.type.match("Weapon") : 
-		GLOBAL.playerData.weapon_id = id
-		return true
+		if GLOBAL.playerData.weapon_id == -1 :
+			GLOBAL.playerData.weapon_id = id
+			return true
+		else : 
+			#do swipe the equipment 
+			print("swipe the equipment done")
 	elif data.type.match("Equipment") : 
-		GLOBAL.playerData.bodyware_id = id
-		return true
+		if GLOBAL.playerData.bodyware_id == -1 :
+			GLOBAL.playerData.bodyware_id = id
+			return true
+		else : 
+			#do swipe the equipment 
+			print("swipe the equipment done")
 	
 	return false
 
 func _on_eqSlot_pressed(extra_arg_0):
 	
+	var playerData = GLOBAL.playerData
+	var prevEqId = -1
 	#should unequip the wearing item
-	
-	
 	match (extra_arg_0):
 		1:
-			GLOBAL.playerData.headware_id = -1
+			prevEqId = playerData.headware_id
+			playerData.headware_id = -1
 		2:
-			GLOBAL.playerData.bodyware_id = -1
+			prevEqId = playerData.bodyware_id
+			playerData.bodyware_id = -1
 		3:
-			GLOBAL.playerData.weapon_id = -1
+			prevEqId = playerData.weapon_id
+			playerData.weapon_id = -1
 		4:
-			GLOBAL.playerData.sheld_id = -1
+			prevEqId = playerData.sheld_id
+			playerData.sheld_id = -1
 			
-	var slot = slots[extra_arg_0 -1] as TouchScreenButton
-	slot.hide()	
-	print("equipment slot - "+str(extra_arg_0)+" unequiped")
 	
+	var allItems = getItems()
+	if allItems:
+		for nItem in allItems:
+			if nItem.type == "Equipment" || nItem.type == "Weapon":
+				if isWearingEquipment(nItem) && nItem.id == prevEqId:
+					nItem.equiped = false
+					(slots[extra_arg_0 -1] as TouchScreenButton ).hide()
+					print("equipment slot - "+str(extra_arg_0)+" unequiped")
+						
+	update()
+	
+	pass # Replace with function body.
+
+
+func _on_btn_filter_button_down(extra_arg_0):
+	if curSelectingType != extra_arg_0 :
+		curSelectingType = extra_arg_0
+		update()
 	pass # Replace with function body.
